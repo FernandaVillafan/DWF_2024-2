66dc0bd783b2de83cc629f031d9a7ab7e9c49da8
TenochFlores <75498092+TenochFlores@users.noreply.github.com>
2024-03-14 13:11:02 -0600

    Practica 6 Finalizada

---
+++ flowermart/src/app/module/product/_service/category.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Category } from '../_model/category/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor() { }
+
+  getCategories(): Category[] {
+    const categories: Category[] = [
+      new Category(1,"Tecnologia","TECH", 1),
+      new Category(2,"Deportes","SPT", 1),
+      new Category(3,"Alimentos","FOOD", 1),
+    ];
+    return categories;
+  }
+}
