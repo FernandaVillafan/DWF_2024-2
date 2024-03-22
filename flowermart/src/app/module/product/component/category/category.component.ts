ab3b787cd1e7e846f74089fddc0ee6398fbf7c79
TenochFlores <75498092+TenochFlores@users.noreply.github.com>
2024-03-22 01:10:53 -0600

    Practica 7 Funcionalidad completa

--- flowermart/src/app/module/product/component/category/category.component.ts
+++ flowermart/src/app/module/product/component/category/category.component.ts
@@ -1,27 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoryService } from '../../_service/category.service';
 import { Category } from '../../_model/category/category';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrls: ['./category.component.css']
 })
-
 export class CategoryComponent implements OnInit {
-
-  categoryService: CategoryService;
+  form: FormGroup;
   categories: Category[] = [];
+  submitted = false; // Añadir una variable para rastrear si el formulario ha sido enviado
 
-  constructor(categoryService: CategoryService) {
-    this.categoryService = categoryService;
-  }
-
-  getCategories(): Category[] {
-    return this.categoryService.getCategories();
+  constructor(private formBuilder: FormBuilder, private categoryService: CategoryService) {
+    this.form = this.formBuilder.group({
+      category: ['', Validators.required],
+      acronym: ['', Validators.required]
+    });
   }
 
   ngOnInit(): void {
-    this.categories = this.getCategories();
+    this.categories = this.categoryService.getCategories();
   }
+
+  onSubmit(): void {
+    this.submitted = true; // Marcar el formulario como enviado para mostrar errores si es necesario
+    if (this.form.valid) {
+      const newCategory: Category = {
+        category_id: this.categories.length + 1, // O alguna otra lógica para generar el ID
+        category: this.form.value.category,
+        acronym: this.form.value.acronym,
+        status: 1 // Suponiendo que 1 representa una categoría activa
+      };
+      this.categories.push(newCategory);
+      // Mostrar mensaje de confirmación
+      alert('Categoría agregada con éxito'); // Considera usar una librería de alertas para un mejor UI
+      this.form.reset();
+      this.submitted = false; // Resetear el estado de enviado
+    }
+  }
+
 }
--- flowermart/src/app/module/product/product.module.ts
+++ flowermart/src/app/module/product/product.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { CategoryComponent } from './component/category/category.component';
 
 @NgModule({
@@ -7,7 +8,8 @@ import { CategoryComponent } from './component/category/category.component';
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule 
   ]
 })
 
