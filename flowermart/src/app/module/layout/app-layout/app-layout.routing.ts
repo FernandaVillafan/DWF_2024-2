f9f4e437dde22d6e0b4e3af7d83e670ff0b25331
Fernanda Villafan <fernanda_flores_25@comunidad.unam.mx>
2024-04-18 13:38:37 -0600

    update: Se completó la parte 2 de la práctica 9

--- flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
+++ flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from '../../product/component/category/category.component';
+import { ProductComponent } from '../../product/component/product/product.component';
 import { SecuredComponent } from '../../authentication/secured/secured.component';
 import { authenticationGuard } from '../../authentication/_guard/authentication.guard';
 
 export const AppLayoutRoutes: Routes = [
     { path: '', component: CategoryComponent },
     { path: 'category', component: CategoryComponent },
+    { path: 'product', component: ProductComponent },
     { path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard] }
 ];
--- flowermart/src/app/module/product/product.module.ts
+++ flowermart/src/app/module/product/product.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { CategoryComponent } from './component/category/category.component';
+import { ProductComponent } from './component/product/product.component';
 
 @NgModule({
   declarations: [
-    CategoryComponent
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
