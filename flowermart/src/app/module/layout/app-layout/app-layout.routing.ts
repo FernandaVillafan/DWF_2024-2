e29ddc69c3723ea0cf4de011e866827ced820e9c
Fernanda Villafan <fernanda_flores_25@comunidad.unam.mx>
2024-04-18 13:55:30 -0600

    fix: Quité que saliera la tabla de categorías en la vista que será el Home de la tienda

--- flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
+++ flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
@@ -5,7 +5,6 @@ import { SecuredComponent } from '../../authentication/secured/secured.component
 import { authenticationGuard } from '../../authentication/_guard/authentication.guard';
 
 export const AppLayoutRoutes: Routes = [
-    { path: '', component: CategoryComponent },
     { path: 'category', component: CategoryComponent },
     { path: 'product', component: ProductComponent },
     { path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard] }
