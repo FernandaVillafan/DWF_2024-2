c5e24f7e1851a465057f90fec31c62b53efb83c9
TenochFlores <75498092+TenochFlores@users.noreply.github.com>
2024-05-06 22:49:57 -0600

    Pagina Home Terminada

--- flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
+++ flowermart/src/app/module/layout/app-layout/app-layout.routing.ts
@@ -4,10 +4,13 @@ import { ProductComponent } from '../../product/component/product/product.compon
 import { SecuredComponent } from '../../authentication/secured/secured.component';
 import { authenticationGuard } from '../../authentication/_guard/authentication.guard';
 import { ProductDetailsComponent } from '../../product/component/product-details/product-details.component';
+import { HomeComponent } from '../../product/component/home/home.component';
 
 export const AppLayoutRoutes: Routes = [
     { path: 'category', component: CategoryComponent },
     { path: 'product', component: ProductComponent },
     { path: 'product/:gtin', component: ProductDetailsComponent },
+    { path: '', component: HomeComponent },
     { path: 'secured', component: SecuredComponent, canActivate : [authenticationGuard] },
+
 ];
